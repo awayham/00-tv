7a39782102c4f46e811118f491b795817837c8fb
awayham <35579408+awayham@users.noreply.github.com>
2021-08-07 16:21:47 +0200

    Add files via upload

---
+++ Iptv-Ayham/Iptv-Ayham/Iptv-Ayham/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Iptv-Ayham
+//
+//  Created by Awad Ayham on 07.08.21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
